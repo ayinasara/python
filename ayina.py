a6245786082d4c8f05373986272ad0f27bc68e52
ayinasara <ayinasarajacob10@gmail.com>
2024-10-04 16:06:51 +0530

    Add files via upload

---
+++ ayina.py
@@ -0,0 +1,43 @@
+# print("Hello")
+# print("World!")
+# print(" hello \n world")
+
+# if 5>3:
+#  print("five is greater than two")
+# if 5 > 3:
+#     print("five is greater than two")
+
+
+
+x=int(3)
+print(type(x))
+
+#NO SPACING BETWEEN VARIABLES
+#KEYWORDS CANNOT BE  USED AS VARIABLES
+'''
+
+x,y,z="orange" , "apple","banana"
+print(x)
+print(y)
+print(z)
+
+x='python'
+y=" is"
+z=" awesome"
+print(x+y+z)
+# input asking user details 
+username=input("enter your username:")
+print("username is :"+ username)
+'''
+'''
+a="hello"
+b="world"
+c=a+b
+print(c)
+'''
+a="Hello, World!"
+print(a[2])
+a="Hello, World!"
+print(a[2:5])#slicing - take values from 2nd upto 4th place
+a="Hello, World!"
+print(a[:5])
