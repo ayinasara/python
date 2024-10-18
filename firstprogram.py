15276e20326bc9251dfdcba553e9d6eb28b6a22a
ayinasara <ayinasarajacob10@gmail.com>
2024-10-18 14:22:51 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,8 @@
+'''
+Author:Ayina
+Date:18-10-24
+Description:After receiving the input, display a message that includes the user's name and age.
+'''
+name=input("enter your name:")
+age=input("enter your age:")
+print("Hello,",name+"! You are",age ,"years old")
