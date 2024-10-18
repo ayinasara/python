b44b6e47840ea3acc8fdec2e928012cf9d7d82a9
ayinasara <ayinasarajacob10@gmail.com>
2024-10-18 15:07:44 +0530

    Add files via upload

---
+++ helloworldsubstring.py
@@ -0,0 +1,9 @@
+'''
+Author:Ayina
+18-10-24
+description: program that stores a string in a variable. Extract a specific part
+'''
+str_variable="Hello, World!"
+str_variable_another=str_variable[7:12]
+str_variable3="Everyone!"
+print(str_variable_another+str_variable3)
