abe19a800e9ef1e756cfdd7ab1aadb1daba281bd
ayinasara <ayinasarajacob10@gmail.com>
2024-10-19 15:39:32 +0530

    Add files via upload

---
+++ inputnumbers.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter num1"))
+num2=int(input("Enter num2"))
+num3=int(input("Enter num3"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+difference=num2-num1
+print("The difference when num2 is subtracted from num1 is",difference)
+product=num1*num2
+print("The product of num1 and num2 is",product)
+division=num1/num2
+print("The quotient when num1 is divided by num2 is",division)
+modulus=num1%num2
+print("The remainder when num1 is divided by num2 is",modulus)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is",result)
