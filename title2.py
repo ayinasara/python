0d51610fffc5099d06507603c24d423127e59227
ayinasara <ayinasarajacob10@gmail.com>
2024-10-19 15:39:54 +0530

    Add files via upload

---
+++ title2.py
@@ -0,0 +1,10 @@
+str1=input("enter your first name")
+str2=input("enter your last name")
+str3=str1+" "+str2
+print(str3)
+b=len(str1)
+print(b)
+str_another=str3[len(str1)+1:]
+print(str_another)
+str_another2=str3[:len(str1)]
+print(str_another2)
